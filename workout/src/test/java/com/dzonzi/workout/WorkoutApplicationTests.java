a4b3529906db5c2836c0002b3b856ee8be1bbcdd
Dz0nZ1 <dzonzi777@gmail.com>
2023-08-26 20:19:36 +0200

    Created spring project

---
+++ workout/src/test/java/com/dzonzi/workout/WorkoutApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dzonzi.workout;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkoutApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
