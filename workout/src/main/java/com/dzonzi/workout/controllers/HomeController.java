937fc4936cae71a73f5167743969665b54d8fc7f
Dz0nZ1 <dzonzi777@gmail.com>
2023-08-26 20:33:30 +0200

    Created HomeController and added lombok dependency

---
+++ workout/src/main/java/com/dzonzi/workout/controllers/HomeController.java
@@ -0,0 +1,18 @@
+package com.dzonzi.workout.controllers;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(name = "/")
+public class HomeController {
+
+
+    @GetMapping(name = "/")
+    public ResponseEntity<String> HomePage() {
+        return new ResponseEntity<>("nesto", HttpStatus.OK);
+    }
+
+}
