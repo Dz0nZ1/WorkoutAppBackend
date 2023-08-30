c3f54f43d77fa6224bde61c5b7ebc4b0d64d24e9
Nikola Leleković <132832709+Dz0nZ1@users.noreply.github.com>
2023-08-30 04:54:53 +0200

    Delete workout directory

--- workout/src/main/java/rs/ac/singidunum/workout/enums/TokenTypeEnum.java
+++
@@ -1,5 +0,0 @@
-package rs.ac.singidunum.workout.enums;
-
-public enum TokenTypeEnum {
-  BEARER
-}
--- workout/src/main/java/rs/ac/singidunum/workout/exceptions/UserNotFoundException.java
+++
@@ -1,9 +0,0 @@
-package rs.ac.singidunum.workout.exceptions;
-
-public class UserNotFoundException extends RuntimeException{
-    public UserNotFoundException(String message){
-        super(message);
-    }
-
-
-}
