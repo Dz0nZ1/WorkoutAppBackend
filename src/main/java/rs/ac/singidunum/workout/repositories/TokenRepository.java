84a96d7cc812e204dd19e832251232bdb7f6b611
Dz0nZ1 <dzonzi777@gmail.com>
2023-09-13 21:47:01 +0200

    Moved to auth package

--- src/main/java/rs/ac/singidunum/workout/repositories/TokenRepository.java
+++ src/main/java/rs/ac/singidunum/workout/repositories/TokenRepository.java
@@ -3,7 +3,7 @@ package rs.ac.singidunum.workout.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import rs.ac.singidunum.workout.models.TokenModel;
+import rs.ac.singidunum.workout.models.auth.TokenModel;
 
 import java.util.List;
 import java.util.Optional;
--- src/main/java/rs/ac/singidunum/workout/repositories/UserRepository.java
+++ src/main/java/rs/ac/singidunum/workout/repositories/UserRepository.java
@@ -2,7 +2,7 @@ package rs.ac.singidunum.workout.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import rs.ac.singidunum.workout.models.UserModel;
+import rs.ac.singidunum.workout.models.auth.UserModel;
 
 @Repository
 public interface UserRepository extends JpaRepository<UserModel, Long> {
