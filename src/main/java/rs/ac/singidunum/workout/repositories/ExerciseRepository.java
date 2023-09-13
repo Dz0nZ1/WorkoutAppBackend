1c8bad9b75806475979b40afbdd7def34b72a574
Dz0nZ1 <dzonzi777@gmail.com>
2023-09-13 21:27:20 +0200

    Created ExerciseRepository

---
+++ src/main/java/rs/ac/singidunum/workout/repositories/ExerciseRepository.java
@@ -0,0 +1,13 @@
+package rs.ac.singidunum.workout.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import rs.ac.singidunum.workout.models.workouts.ExerciseModel;
+
+@Repository
+public interface ExerciseRepository extends JpaRepository<ExerciseModel, Long> {
+
+    ExerciseModel findByName(String name);
+
+
+}
