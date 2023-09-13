019328695bb06e25b12e291f3ccd716063c6605b
Dz0nZ1 <dzonzi777@gmail.com>
2023-09-13 21:26:25 +0200

    Created ExerciseController

---
+++ src/main/java/rs/ac/singidunum/workout/controllers/admin/ExerciseController.java
@@ -0,0 +1,73 @@
+package rs.ac.singidunum.workout.controllers.admin;
+
+
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+import rs.ac.singidunum.workout.exceptions.InvalidArgumentsHandler;
+import rs.ac.singidunum.workout.models.workouts.ExerciseModel;
+import rs.ac.singidunum.workout.services.exercises.ExerciseService;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/api/v1/exercise")
+@PreAuthorize("hasRole('Admin')")
+@RequiredArgsConstructor
+public class ExerciseController {
+
+    private final ExerciseService exerciseService;
+
+    @GetMapping("/all")
+    @PreAuthorize("hasAuthority('admin:read')")
+    public ResponseEntity<List<ExerciseModel>> getAllExercises(){
+        return new ResponseEntity<>(exerciseService.gelAllExercises(), HttpStatus.OK);
+    }
+
+    @GetMapping("/get/{id}")
+    @PreAuthorize("hasAuthority('admin:read')")
+    public ResponseEntity<ExerciseModel> getExercise(@PathVariable("id") Long exerciseId){
+        return new ResponseEntity<>(exerciseService.getExercise(exerciseId), HttpStatus.OK);
+    }
+
+    @PostMapping("/create")
+    @PreAuthorize("hasAuthority('admin:create')")
+    public ResponseEntity<?> createExercise(@Valid @RequestBody ExerciseModel exerciseModel, BindingResult result) {
+
+        if(result.hasErrors()) {
+            var exceptionHandler = new InvalidArgumentsHandler();
+            return new ResponseEntity<>(exceptionHandler.getErrorMessages(result), HttpStatus.BAD_REQUEST);
+        }
+
+        return new ResponseEntity<>(exerciseService.createExercise(exerciseModel), HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    @PreAuthorize("hasAuthority('admin:delete')")
+    public ResponseEntity<Map<String, String>> delete(@PathVariable("id") Long userId) {
+    exerciseService.deleteExercise(userId);
+        Map<String, String> response = new HashMap<>();
+        response.put("defaultMessage", "Exercise was deleted");
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+
+    @PutMapping("/update/{id}")
+    @PreAuthorize("hasAuthority('admin:update')")
+    public ResponseEntity<?> updateExercise(@Valid @RequestBody ExerciseModel exerciseModel, @PathVariable("id") Long exerciseId , BindingResult result){
+
+        if(result.hasErrors()) {
+            var exceptionHandler = new InvalidArgumentsHandler();
+            return new ResponseEntity<>(exceptionHandler.getErrorMessages(result), HttpStatus.BAD_REQUEST);
+        }
+
+        return  new ResponseEntity<>(exerciseService.updateExercise(exerciseModel, exerciseId), HttpStatus.OK);
+    }
+
+}
