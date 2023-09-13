6ad1440746eff79da097e11a7eed73efd6f57409
Dz0nZ1 <dzonzi777@gmail.com>
2023-09-13 21:28:52 +0200

    Created ExerciseService

---
+++ src/main/java/rs/ac/singidunum/workout/services/exercises/ExerciseService.java
@@ -0,0 +1,22 @@
+package rs.ac.singidunum.workout.services.exercises;
+
+import rs.ac.singidunum.workout.exceptions.ExerciseNotFoundException;
+import rs.ac.singidunum.workout.models.workouts.ExerciseModel;
+
+import java.util.List;
+
+public interface ExerciseService {
+
+    List<ExerciseModel> gelAllExercises();
+
+    ExerciseModel createExercise(ExerciseModel exerciseModel);
+
+    ExerciseModel getExercise(Long exerciseId) throws ExerciseNotFoundException;
+
+    ExerciseModel findByName(String name);
+
+    void deleteExercise(Long exerciseId);
+
+    ExerciseModel updateExercise(ExerciseModel exerciseModel, Long exerciseId);
+
+}
