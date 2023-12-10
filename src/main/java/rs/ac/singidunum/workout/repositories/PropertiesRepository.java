242ad6c2ea3da3b744e8a3824a95410bc8f9b641
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-10 01:15:56 +0100

    Created findAllByPlan method in PropertiesRepository

--- src/main/java/rs/ac/singidunum/workout/repositories/PropertiesRepository.java
+++ src/main/java/rs/ac/singidunum/workout/repositories/PropertiesRepository.java
@@ -2,6 +2,7 @@ package rs.ac.singidunum.workout.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import rs.ac.singidunum.workout.models.workouts.PlanModel;
 import rs.ac.singidunum.workout.models.workouts.PropertyModel;
 
 import java.util.List;
@@ -9,4 +10,6 @@ import java.util.List;
 @Repository
 public interface PropertiesRepository extends JpaRepository<PropertyModel, Long> {
     List<PropertyModel> findAllByForExercise(String forExercise);
+
+    List<PropertyModel> findAllByPlan(PlanModel plan);
 }
