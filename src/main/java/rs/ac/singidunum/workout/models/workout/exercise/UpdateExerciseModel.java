d9d57f55d61fa8edc35b3945c9eae291edaa431b
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-16 19:58:19 +0100

    Created UpdateExerciseModel

---
+++ src/main/java/rs/ac/singidunum/workout/models/workout/exercise/UpdateExerciseModel.java
@@ -0,0 +1,16 @@
+package rs.ac.singidunum.workout.models.workout.exercise;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class UpdateExerciseModel {
+    private String name;
+    private String photo;
+    private String category;
+}
