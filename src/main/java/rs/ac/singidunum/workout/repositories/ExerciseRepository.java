d275d9a6c81d072a4052156eaa50ab9624374351
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-30 16:10:38 +0100

    Created a new method in ExerciseRepository for fetching set of exercises

--- src/main/java/rs/ac/singidunum/workout/repositories/ExerciseRepository.java
+++ src/main/java/rs/ac/singidunum/workout/repositories/ExerciseRepository.java
@@ -1,12 +1,17 @@
 package rs.ac.singidunum.workout.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 import rs.ac.singidunum.workout.entities.workouts.Exercise;
+
+import java.util.Set;
 
 @Repository
 public interface ExerciseRepository extends JpaRepository<Exercise, Long> {
 
+    @Query(value ="SELECT * FROM exercises" ,nativeQuery = true)
+    Set<Exercise> findAllSet();
     Exercise findByName(String name);
 
     void deleteByName(String name);
