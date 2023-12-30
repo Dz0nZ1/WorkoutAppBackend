a49488e53970cedc8407ee0257dd293c380bed2b
Dz0nZ1 <dzonzi777@gmail.com>
2023-12-30 16:13:15 +0100

    Added a set of exercises in UpdatePlanModel

--- src/main/java/rs/ac/singidunum/workout/models/workout/plan/UpdatePlanModel.java
+++ src/main/java/rs/ac/singidunum/workout/models/workout/plan/UpdatePlanModel.java
@@ -4,6 +4,9 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import rs.ac.singidunum.workout.models.workout.exercise.ExerciseModel;
+
+import java.util.Set;
 
 @Data
 @AllArgsConstructor
@@ -11,4 +14,6 @@ import lombok.NoArgsConstructor;
 @Builder
 public class UpdatePlanModel {
     private String name;
+    private Set<ExerciseModel> exercises;
+
 }
